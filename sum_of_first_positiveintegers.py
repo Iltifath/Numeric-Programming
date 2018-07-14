d17d3a6d0d8858cabc95a989f99ef1c61dfc5e56
ILTIFATH AHMED <37118293+Iltifath@users.noreply.github.com>
2018-07-14 12:07:35 +0530

    problem 1
    This code will ask to enter a number(n) and finds the sum of first 'n' positive integers.

---
+++ sum_of_first_positiveintegers.py
@@ -0,0 +1,8 @@
+#Sum of the First n Positive Integers
+
+#Get the number
+n=int(input('Enter the number: '))
+#Find the Sum 
+sum=(n*(n+1))/2
+#Print the output.
+print('The sum of first %d positive integers is %d' %(n,sum))
